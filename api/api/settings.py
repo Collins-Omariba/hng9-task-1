663cf508aebb17c41d7a1f8a60424174fe239e76
Collins Omariba <collinsomariba@gmail.com>
2022-10-28 13:57:34 +0300

    Set cors_allow_all_origins to True

--- api/api/settings.py
+++ api/api/settings.py
@@ -55,21 +55,24 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:3000',
-    'http://127.0.0.1:3030',
-    'http://127.0.0.1:8000',
-    'http://collinsoma.pythonanywhere.com',
-]
+
+CORS_ALLOW_ALL_ORIGINS: True
+
+# CORS_ALLOWED_ORIGINS = [
+#     'http://127.0.0.1:3000',
+#     'http://127.0.0.1:3030',
+#     'http://127.0.0.1:8000',
+#     'http://collinsoma.pythonanywhere.com',
+# ]
 ROOT_URLCONF = 'api.urls'
 
 TEMPLATES = [
